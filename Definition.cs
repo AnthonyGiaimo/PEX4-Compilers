abe772b7a1dd2e5fcf3db35a75e3eac223e614a2
AnthonyGiaimo <36255203+AnthonyGiaimo@users.noreply.github.com>
2019-11-07 09:23:52 -0700

    Add files via upload

---
+++ Definition.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ToyLanguage
+{
+    public abstract class Definition
+    {
+        public string name;
+    }
+    public abstract class TypeDefinition : Definition
+    {
+    }
+    public class BasicTypeDefinition : TypeDefinition
+    {
+    }
+    public class StringTypeDefinition : TypeDefinition
+    {
+    }
+    public class VariableDefinition : Definition
+    {
+        public TypeDefinition vartype;
+    }
+    public class MethodDefinition : Definition
+    {
+        public List<VariableDefinition> paramList;
+
+    }
+}
---
+++ SemanticAnalyzer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ToyLanguage.node;
+
+namespace ToyLanguage.analysis
+{
+    class SemanticAnalyzer : DepthFirstAdapter
+    {
+        
+    }
+}
+
+
+//  Start -> id + start;
